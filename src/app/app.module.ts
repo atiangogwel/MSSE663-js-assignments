d32e144ffc9fd07655ce98022b6a1fb6d493725a
pc <pc@localhost.localdomain>
2023-08-31 03:31:19 +0300

    Update code for week 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PizzaAppComponent } from './pizza-app/pizza-app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, PizzaAppComponent],
+  imports: [BrowserModule, AppRoutingModule], // Import the AppRoutingModule
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pizza-app/pizza-app.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzaAppComponent } from './pizza-app.component';
+
+describe('PizzaAppComponent', () => {
+  let component: PizzaAppComponent;
+  let fixture: ComponentFixture<PizzaAppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PizzaAppComponent]
+    });
+    fixture = TestBed.createComponent(PizzaAppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
